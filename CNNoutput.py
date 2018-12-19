d004b6764b9cbbeda9a73d6e255d44cc1a2ae73c
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2018-12-19 05:35:52 -0600

    Update CNNoutput.py

--- CNNoutput.py
+++ CNNoutput.py
@@ -1,36 +1,60 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 16 13:52:55 2018
-
-@author: Leonardo
-"""
-
+# --------------------------------------------------------
+# IMPORTS:
 from plotly.offline import plot
 import plotly.graph_objs as go
 import numpy as np
+# --------------------------------------------------------
+# --------------------------------------------------------
+# FUNCTIONS:
+def unpackXYZcoords(A):
+    x = [0]*A.shape[0]**2  # empty preallocation
+    y = [0]*A.shape[0]**2  # empty preallocation
+    z = [0]*A.shape[0]**2  # empty preallocation
+    
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            x[i*A.shape[1] + j] = i
+            y[i*A.shape[1] + j] = j
+            z[i*A.shape[1] + j] = A[i][j]
+    return x,y,z
 
 
-f = 5
-p = 2
-s = 1
-dud = np.ones((1,101))
-n = np.linspace(0,1000,101)
-Size = (n + 2*p - f)//s + 1
 
-# ratio of n_output/n_input
-Expansion = Size/n
+def findExpansion(f,s):
+    # let's pass through a fixed input size of 10:
+    n_In = 1000
+    # same padding:
+    p = f//2
+    n_Out = (n_In + 2*p - f)//s + 1
+    
+    Expansion = n_Out/n_In
+    
+    return Expansion
+    
+# --------------------------------------------------------
 
-x = list(dud.T)
-y = list(n)
-z = list(2*n + 20)
 
+f = np.linspace(1,100,100)
+s = np.linspace(1,100,100)
+
+A = np.zeros((len(f), len(s)))
+
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        A[i][j] = findExpansion(f[i], s[j])  # store expansion based on filter and stride
+
+#print(A)
+
+"""
+x, y, z = unpackXYZcoords(A)
 trace1 = go.Scatter3d(
     x=x,
     y=y,
     z=z,
     mode='markers',
     marker=dict(
-        size=12,
+        size=1,
         line=dict(
             color='rgba(217, 217, 217, 0.14)',
             width=0.5
@@ -49,8 +73,31 @@ layout = go.Layout(
     )
 )
 fig = go.Figure(data=data, layout=layout)
+"""
 
+data = [
+    go.Surface(
+        z=A
+    )
+]
+layout = go.Layout(
+    title='CNN Output/Input Sizes',
+    autosize=True,  # we want this to fill the screen
+#    width=500,
+#    height=500,
+    margin=dict(
+        l=65,
+        r=50,
+        b=65,
+        t=90
+    ),
+    xaxis=dict(
+        title='Filter Size'
+    ),
+    yaxis=dict(
+        title='Stride Length'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
 
 plot(fig)
- 
-
